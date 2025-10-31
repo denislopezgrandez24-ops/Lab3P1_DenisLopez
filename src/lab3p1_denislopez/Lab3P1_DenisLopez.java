0306c74fa6effc7738e33a9bd4974e9eab381243
Denis J. Lopez G. <denislopezgrandez24@gmail.com>
2025-10-31 13:39:12 -0600

    avance con el primer ejercicio

--- src/lab3p1_denislopez/Lab3P1_DenisLopez.java
+++ src/lab3p1_denislopez/Lab3P1_DenisLopez.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package lab3p1_denislopez;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +15,27 @@ public class Lab3P1_DenisLopez {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner (System.in);
+        System.out.println("Menu");
+        System.out.println("1. Figuras y mas figuras");
+        System.out.println("2. Te dije que te llevaras lo que quisieras, no que te llevaras todo");
+        System.out.println("3. Sumatoria");
+        System.out.println("4. Salir");
+        int opcion= leer.nextInt();
+        while (opcion!=4){
+            
+            switch(opcion){
+                
+                case 1:{
+                    System.out.println("Figuras y mas figuras");
+                    
+                    
+                }//fin opcion 1
+                
+            }// fin del switch
+            
+        }// fin del while
+        
     }
     
 }
