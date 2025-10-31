711aca5bb457bcf057cf6e4dec659d28c9705933
Denis J. Lopez G. <denislopezgrandez24@gmail.com>
2025-10-31 14:25:06 -0600

    ejecicio 1 en progreso

--- src/lab3p1_denislopez/Lab3P1_DenisLopez.java
+++ src/lab3p1_denislopez/Lab3P1_DenisLopez.java
@@ -21,6 +21,7 @@ public class Lab3P1_DenisLopez {
         System.out.println("2. Te dije que te llevaras lo que quisieras, no que te llevaras todo");
         System.out.println("3. Sumatoria");
         System.out.println("4. Salir");
+        System.out.println("Ingrese su opcion: ");
         int opcion= leer.nextInt();
         while (opcion!=4){
             
@@ -28,9 +29,29 @@ public class Lab3P1_DenisLopez {
                 
                 case 1:{
                     System.out.println("Figuras y mas figuras");
-                    
+                    System.out.println("Ingrese un numero par mayor o igual a 6");
+                    int par = leer.nextInt();
+                    if (par%2==0 && par>=6){
+                        for (int i=1; i<=par; i++){
+                            for(int j=1; j<=par; j++){
+                                    System.out.print("- ");
+                                    if ((i==1 && i==par)&& (j==1 && j))
+                            }
+                            
+                            System.out.println(" ");
+                        }//
+                        System.out.println();
+                    }
+                    System.out.println("Tamano invalido intente denuevo:");
+                    par = leer.nextInt();
                     
                 }//fin opcion 1
+                break;
+                case 2:{
+                    System.out.println("Te dije que te llevaras lo que quisieras, no que te llevaras todo");
+                    
+                    
+                }
                 
             }// fin del switch
             
